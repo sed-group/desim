f23f666aacc55916dcd1ee3693d9f0c41fc3b4dc
EppChops <erik.00.berg@gmail.com>
2022-08-02 10:33:49 +0200

    Basic setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(name='desim',
+version='0.1.0',
+description='A discrete event simulator',
+url='https://github.com/EppChops/event-sim',
+author='Erik',
+install_requires=['simpy', 'numpy'],
+author_email='ex',
+packages=setuptools.find_packages())
