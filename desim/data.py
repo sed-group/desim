0546893db22e7574304b49600bd82fe4f7127a9d
EppChops <erik.00.berg@gmail.com>
2022-08-05 17:08:13 +0200

    Added simresults class to handle calculations of the results from the simulations

--- desim/data.py
+++ desim/data.py
@@ -1,4 +1,7 @@
+from dataclasses import dataclass
+from typing import List
 from enum import Enum
+import numpy as np
 
 class NonTechCost(Enum):
     """
@@ -20,3 +23,65 @@ class TimeFormat(Enum):
     WEEK = 52
     MONTH = 12
     YEAR = 1
+
+@dataclass(unsafe_hash=True)
+class SimResults:
+    """
+    Class for saving the results from a simulation and doing simple
+    calculations on the results
+    """
+    design: str
+    processes: List
+    timesteps: List[List[float]]
+    npvs: List[List[float]]
+    costs: List[List[float]]
+    revenues: List[List[float]]
+
+    def normalize_npv(self):
+        normalized_npv = []
+
+        mean_npv = self.mean_npv()
+        max_npv = max(mean_npv)
+        min_npv = min(mean_npv)
+
+        delta_npv = max_npv - min_npv
+        for npv in mean_npv:
+            n_npv = (npv - min_npv) / delta_npv
+            normalized_npv.append(n_npv)
+
+        return normalized_npv 
+
+
+    def mean_npv_payback_time(self):
+        """
+        Calculates the time it takes before the mean npv 
+        starts to generate value. If the mean NPV doesn't
+        go above 0 then -1 is returned. 
+        """
+        
+        for t, n in zip(self.timesteps[-1], self.mean_npv()):
+            if n >= 0:
+                return t
+        
+        return -1
+
+    def mean_npv(self):
+        mean_npvs = []
+        for _cumulative_npv in np.array(self.npvs).transpose():
+            mean_npvs.append(np.mean(_cumulative_npv))
+        
+        return mean_npvs
+    
+    def mean_costs(self):
+        mean_costs = []
+        for costs in np.array(self.costs).transpose():
+            mean_costs.append(np.mean(costs))
+        
+        return mean_costs
+    
+    def mean_revenues(self):
+        mean_revenues = []
+        for revenues in np.array(self.revenues).transpose():
+            mean_revenues.append(np.mean(revenues))
+
+        return mean_revenues
--- desim/interface.py
+++ desim/interface.py
@@ -2,8 +2,9 @@ from typing import List
 
 import numpy as np
 import multiprocessing as mp
-
+from desim.data import SimResults
 from desim.simulation import Process, Simulation, Entity, NonTechCost
+
 
 class Des(object):
     def __init__(self) -> None:
@@ -18,6 +19,7 @@ class Des(object):
         sim = Simulation(flow_time, flow_rate, flow_start_process, until, discount_rate,
                      processes, non_tech_processes, non_tech_costs, dsm)
         sim.run_simulation()
+
 
         return sim.time_steps, sim.cum_NPV, sim.total_costs, sim.total_revenue
 
@@ -39,20 +41,10 @@ class Des(object):
             total_costs.append(total_c)
             total_revenue.append(total_r)
         
-        mean_timesteps = []
-        mean_npvs = []
-        mean_costs = []
-        mean_revenue = []
-
-        for ts, npvs, costs, revenue in np.array(time_steps).transpose(), np.array(cumulative_NPV).transpose(), np.array(total_costs).transpose(), np.array(total_revenue).transpose():
-            mean_timesteps.append(np.mean(ts))
-            mean_npvs.append(np.mean(npvs))
-            mean_costs.append(np.mean(costs))
-            mean_revenue.append(np.mean(revenue))
         
-
-        return mean_timesteps, mean_npvs, mean_costs, mean_revenue
-    
+        
+        return SimResults('No Design', processes, time_steps, cumulative_NPV, total_costs, total_revenue)
+ 
     def run_parallell_simulations(self, flow_time: float, flow_rate: float, 
         flow_start_process: Process, processes: List[Process], 
         non_tech_processes, non_tech_costs: NonTechCost, dsm: dict, 
@@ -76,26 +68,5 @@ class Des(object):
             total_costs.append(cost)
             total_revenue.append(revenue)
 
-        mean_timesteps = []
-        mean_npvs = []
-        mean_costs = []
-        mean_revenue = []
 
-
-        for _time_steps in np.array(time_steps).transpose():
-            mean_timesteps.append(np.mean(_time_steps))
-
-        for _cumulative_npv in np.array(cumulative_NPV).transpose():
-            mean_npvs.append(np.mean(_cumulative_npv))
-
-        for _total_costs in np.array(total_costs).transpose():
-            mean_costs.append(np.mean(_total_costs))
-        
-        for _total_revenue in np.array(total_revenue).transpose():
-            mean_revenue.append(np.mean(_total_revenue))
-
-        #transposed = (_time_steps, _cumulative_npv, _total_costs, _total_revenue)
-
-        #for (ts, npvs, costs, revenue) in transposed:
-
-        return mean_timesteps, mean_npvs, mean_costs, mean_revenue
+        return SimResults('No Design', processes, time_steps, cumulative_NPV, total_costs, total_revenue)
