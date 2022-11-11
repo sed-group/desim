4c9980f9180014658714919950996902ce58a03f
EppChops <erik.00.berg@gmail.com>
2022-11-11 12:15:31 +0100

    changed interarrival process to be based on string instead of id

--- desim/interface.py
+++ desim/interface.py
@@ -11,12 +11,12 @@ class Des(object):
         pass
 
     def run_simulation(self, flow_time: float, flow_rate: float, 
-        flow_start_process_id: int, processes: List[Process], 
+        flow_start_process: str, processes: List[Process], 
         non_tech_processes, non_tech_costs: NonTechCost, dsm: dict, 
         discount_rate=0.08, until = 100):
         
         
-        sim = Simulation(flow_time, flow_rate, flow_start_process_id, until, discount_rate,
+        sim = Simulation(flow_time, flow_rate, flow_start_process, until, discount_rate,
                      processes, non_tech_processes, non_tech_costs, dsm)
         sim.run_simulation()
 
@@ -24,7 +24,7 @@ class Des(object):
         return sim.time_steps, sim.cum_NPV, sim.total_costs, sim.total_revenue
 
     def run_monte_carlo_simulation(self, flow_time: float, flow_rate: float, 
-        flow_start_process: int, processes: List[Process], 
+        flow_start_process: str, processes: List[Process], 
         non_tech_processes, non_tech_costs: NonTechCost, dsm: dict, 
         discount_rate=0.08, until = 100, runs=300):
 
@@ -46,7 +46,7 @@ class Des(object):
         return SimResults('No Design', processes, time_steps, cumulative_NPV, total_costs, total_revenue)
  
     def run_parallell_simulations(self, flow_time: float, flow_rate: float, 
-        flow_start_process: int, processes: List[Process], 
+        flow_start_process: str, processes: List[Process], 
         non_tech_processes, non_tech_costs: NonTechCost, dsm: dict, 
         discount_rate=0.08, until = 100, runs=300):
 
