a824ef984e3daccd230de6881127a16fd3ea8d11
Oscar Bennet <oscar.bennet@outlook.com>
2024-06-11 14:29:40 +0200

    0.4.8

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 
 setuptools.setup(name='desim-tool',
-version='0.4.7',
+version='0.4.8',
 description='A discrete event simulator',
 url='https://github.com/sed-group/desim',
 author='Erik Berg, Oscar Bennet',
